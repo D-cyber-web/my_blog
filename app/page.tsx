cb46e288d3a6ddf7645cae1ccc406396fa6a65bd
Davie <muthama3david@gmail.com>
2024-11-11 13:02:40 +0300

    update my-blog

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import { Card } from "@/components/ui/card";
+ import { Card } from "@/components/ui/card";
 import { simpleBlogCard } from "./lib/interface";
-import { client } from "./lib/sanity";
+import { client, urlFor } from "./lib/sanity";
+import Image from "next/image";
 
 async function getData() {
   const query = `*[_type == 'blog'] | order(_createdAt desc){
@@ -18,13 +19,26 @@ export default async function Home() {
   const data: simpleBlogCard[] = await getData();
 
   console.log(data);
+
   return (
     <div className="grid grid-flow-cols-1 lg:grid-cols-4 mt-5">
-      {data.map((post, idx) => (
-        <Card key={idx}>
-          <Image src={urlFor{post.titleImage}.url[]} alt="image" width={500} height={500} />
-        </Card>
-      ))}
-    </div>
+  {data.map((post, idx) => (
+    <Card key={idx}>
+      {post.titleImage ? (
+        <Image 
+          src={urlFor(post.titleImage).url()} 
+          alt="image" 
+          width={500} 
+          height={500} 
+        />
+      ) : (
+        <div style={{ width: 500, height: 500, backgroundColor: "#f0f0f0" }}>
+          No Image
+        </div>
+      )}
+    </Card>
+  ))}
+</div>
+
   );
 }
