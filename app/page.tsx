22872c3088eb0677cf77cfa388a297535aeb65e0
Davie <muthama3david@gmail.com>
2024-11-11 14:09:47 +0300

    uploading the content

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
- import { Card } from "@/components/ui/card";
+ import { Card, CardContent } from "@/components/ui/card";
 import { simpleBlogCard } from "./lib/interface";
 import { client, urlFor } from "./lib/sanity";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 
 async function getData() {
   const query = `*[_type == 'blog'] | order(_createdAt desc){
@@ -21,7 +24,7 @@ export default async function Home() {
   console.log(data);
 
   return (
-    <div className="grid grid-flow-cols-1 lg:grid-cols-4 mt-5">
+    <div className="grid grid-flow-cols-1 md:grid-cols-2 mt-5 gap-5">
   {data.map((post, idx) => (
     <Card key={idx}>
       {post.titleImage ? (
@@ -30,12 +33,22 @@ export default async function Home() {
           alt="image" 
           width={500} 
           height={500} 
+          className="rounded-t-lg h-[170px]"
         />
+        
       ) : (
         <div style={{ width: 500, height: 500, backgroundColor: "#f0f0f0" }}>
           No Image
         </div>
       )}
+      <CardContent className="mt-5">
+          <h3 className="text-xl line-clamp-2 font-bold">{post.title}</h3>
+          <p className="text-sm line-clamp-3 mt-2 text-gray-600 dark:text-gray-300">{post.smallDescription}</p>
+
+          <Button asChild className="w-full mt-7" >
+              <Link href={`/blog/${post.currentSlug}`}>Read More</Link>
+          </Button>
+      </CardContent>
     </Card>
   ))}
 </div>
