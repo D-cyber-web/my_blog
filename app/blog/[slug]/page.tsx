4cd8ff82f2f922ca44d7179dca6fc40df3ce77c3
Davie <muthama3david@gmail.com>
2024-11-12 13:40:18 +0300

    uploading the content

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { fullBlog } from "@/app/lib/interface";
-import { client } from "@/app/lib/sanity";
+import { client, urlFor } from "@/app/lib/sanity";
+import Image from "next/image";
 
 async function getData(slug: string) {
   const query = `
@@ -24,10 +25,21 @@ export default async function BlogArticle({
   const data: fullBlog = await getData(slug);
 
   return (
-    <div>
+    <div className="mt-8">
         <h1>
-            <span className="block text-base text-center text-primary">Davie Muthama - Blog</span>
+            <span className="block text-xl text-center text-primary font-semibold tracking-wide uppercase">
+              Davie Muthama - Blog
+            </span>
+            <span className="mt-2 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl">{data.title}</span>
         </h1>
+
+        <Image 
+          src={urlFor(data.titleImage).url()} 
+          width={800} 
+          height={800} 
+          alt="Title Image" 
+          className="rounded-lg mt-8 border"
+          />
     </div>
-  );
+  ); 
 }
